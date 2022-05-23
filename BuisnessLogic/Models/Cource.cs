80a334edf910967c1b6d3e2adf253440eed9be29
Dmitriy Zharkov <zharkov.de@edu.spbstu.ru>
2022-05-23 19:55:39 +0300

    Поправил модели

---
+++ BuisnessLogic/Models/Cource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuisnessLogic.Models;
+
+public class Cource
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = null!;
+    public int LectorId { get; set; } 
+}
